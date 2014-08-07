2e92b82ab21a2f5e1a94f6794404e76b7263de9c
Ordon <ordon1313@gmail.com>
2014-08-07 20:11:54 +0200

    Implementation removed

--- src/main/java/com/ordonteam/gameoflife/Game.java
+++ src/main/java/com/ordonteam/gameoflife/Game.java
@@ -1,10 +1,5 @@
 package com.ordonteam.gameoflife;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Sets;
-
 import java.awt.*;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -14,7 +9,7 @@ public class Game {
 
     private Set<Life> lives = new HashSet<Life>();
 
-    public Iterable<Life> getLives() {
+    Iterable<Life> getLives() {
         return lives;
     }
 
@@ -27,65 +22,6 @@ public class Game {
     }
 
     public void tick() {
-        Iterable<Life> survived = getSurviving();
-        Iterable<Life> emerged = getEmerged();
-        lives = Sets.newHashSet(Iterables.concat(survived,emerged));
-    }
-
-    private Set<Life> getSurviving() {
-        return Sets.filter(lives, new Predicate<Life>() {
-            @Override
-            public boolean apply(Life life) {
-                return shouldSurvive(life);
-            }
-        });
-    }
-
-    private boolean shouldSurvive(Life life) {
-        int count = getLivingNeighborsCount(life);
-        return count == 2 || count == 3;
-    }
-
-    private int getLivingNeighborsCount(Life life) {
-        Set<Life> livingNeighbors = Sets.filter(life.getPossibleNeighbors(), new Predicate<Life>() {
-            @Override
-            public boolean apply(Life neighbor) {
-                return lives.contains(neighbor);
-            }
-        });
-        return livingNeighbors.size();
-    }
-
-    private Iterable<Life> getEmerged() {
-//        HashSet<Life> emerged = new HashSet<Life>();
-//        for (Life life : lives) {
-//            for (Life possibleToEmerge : life.getPossibleNeighbors()) {
-//                if(shouldEmerged(possibleToEmerge))
-//                    emerged.add(possibleToEmerge);
-//            }
-//        }
-//        return emerged;
-
-        Iterable<Life> allPossibleEmerged = Iterables.concat(Iterables.transform(lives, new Function<Life, Iterable<Life>>() {
-            @Override
-            public Iterable<Life> apply(Life life) {
-                return life.getPossibleNeighbors();
-            }
-        }));
-        Iterable<Life> allEmerged = Iterables.filter(allPossibleEmerged, new Predicate<Life>() {
-            @Override
-            public boolean apply(Life life) {
-                return shouldEmerged(life);
-            }
-        });
-        return allEmerged;
-    }
-
-    private boolean shouldEmerged(Life life) {
-        if(lives.contains(life))
-            return false;
-        int count = getLivingNeighborsCount(life);
-        return count == 3;
     }
 
     public void changeLife(Life life) {
--- src/test/java/com/ordonteam/gameoflife/GameTest.java
+++ src/test/java/com/ordonteam/gameoflife/GameTest.java
@@ -14,86 +14,42 @@ public class GameTest {
 
     @Test
     public void shouldCreateWorld() throws Exception {
-        assertThat(game).isNotNull();
     }
 
     @Test
     public void shouldCreateEmptyWorld() throws Exception {
-        assertThat(game.getLives()).isEmpty();
     }
 
     @Test
     public void shouldAddedLifeBeOnBoard() throws Exception {
-        //given
-        game.addLife(new Life(0,0));
-        //when
-        Iterable<Life> lives = game.getLives();
-        //then
-        assertThat(lives).isNotEmpty();
     }
 
     @Test
     public void shouldLivesBeSameOnTheSameCoordinates() throws Exception {
-        //given
-        game.addLife(new Life(0, 0));
-        //when
-        Iterable<Life> lives = game.getLives();
-        //then
-        assertThat(lives).contains(new Life(0, 0));
     }
 
     @Test
     public void shouldAllAddedLivesBeOnBoard() throws Exception {
-        game.addLives(new Life(0, 0), new Life(0, 1));
-
-        Iterable<Life> lives = game.getLives();
-
-        assertThat(lives).contains(new Life(0,0),new Life(0,1));
     }
 
     @Test
     public void shouldDieWithoutEnoughNeighbors() throws Exception {
-        game.addLives(new Life(0,0));
-
-        game.tick();
-
-        assertThat(game.getLives()).isEmpty();
     }
 
     @Test
     public void shouldSurviveWhit2Neighbors() throws Exception {
-        game.addLives(new Life(0,0),new Life(0,1),new Life(1,0));
-
-        game.tick();
-
-        assertThat(game.getLives()).contains(new Life(0, 0));
     }
 
     @Test
     public void shouldDieWhenFriendsAreTooFar() throws Exception {
-        game.addLives(new Life(0,0),new Life(5,1),new Life(5,0));
-
-        game.tick();
-
-        assertThat(game.getLives()).doesNotContain(new Life(0, 0));
     }
 
     @Test
     public void shouldBeAbleToAddLifeAfterTick() throws Exception {
-        game.tick();
-
-        game.addLives(new Life(0, 0));
-
-        assertThat(game.getLives()).contains(new Life(0, 0));
     }
 
     @Test
     public void shouldEmergeWithThreeNeighbors() throws Exception {
-        game.addLives(new Life(0, 1), new Life(1, 0), new Life(-1, -1));
-
-        game.tick();
-
-        assertThat(game.getLives()).contains(new Life(0, 0));
     }
 
     @Test
